d23fc6b0ab4e91baa08404875d93c53c91fb5a97
unknown <jschueren1431@gmail.com>
2023-05-18 17:08:49 -0700

    initial commit, created form to generate plan and manage user input

---
+++ Fitness-Tracker/Controllers/GenerateController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fitness_Tracker.Controllers
+{
+    public class GenerateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Generate");
+        }
+
+        [HttpPost]
+        public IActionResult Results(int age, int feet, int inches, int weight, string gender, int goal, int activity)
+        {
+            ViewBag.Age = age;
+            ViewBag.Feet = feet;
+            ViewBag.Inches = inches;
+            ViewBag.Weight = weight;
+            ViewBag.Gender = gender;
+            ViewBag.Goal = goal;
+            ViewBag.Activity = activity;
+            
+            return View("Results");
+        }
+    }
+}
---
+++ Fitness-Tracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fitness_Tracker.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
