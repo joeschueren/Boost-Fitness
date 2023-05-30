408b565767073c432d4f92a320a5cf39d65495d3
unknown <jschueren1431@gmail.com>
2023-05-29 21:49:10 -0700

    created the controller to handle Users in the database

--- Fitness-Tracker/Controllers/ProfileController.cs
+++ Fitness-Tracker/Controllers/ProfileController.cs
@@ -1,4 +1,5 @@
 ﻿using Fitness_Tracker.Data;
+using Fitness_Tracker.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,7 +17,42 @@ namespace Fitness_Tracker.Controllers
         [Authorize]
         public IActionResult Index()
         {
+            string username = User.Identity.Name;
+
+            var user = _context.Users.FirstOrDefault(x => x.User == username);
+
+            ViewBag.user = user;
+
+            var days = _context.Days.Where(x => x.User == username).ToList();
+               
+            ViewBag.days = days;
+
+            _context.Dispose();
+
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Input(string User, int weight, int feet, int inches,  int age, string gender)
+        {
+            int height = (feet * 12) + inches;
+
+            UserInfo newUser = new UserInfo
+            {
+                User = User,
+                Weight = weight,
+                Height = height,
+                Age = age,
+                Gender = gender,
+                TableReady = true
+
+            };
+
+            _context.Users.Add(newUser);
+
+            _context.Dispose();
+
+            return RedirectToAction("Index", "Profile");
         }
     }
 }
